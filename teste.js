7c387a786ce9718c0f48c84ce83dac440f36ee7a
pcezar1118 <pcezar1118@gmail.com>
2022-11-18 11:59:41 -0200

    sync

--- teste.js
+++ teste.js
@@ -4,8 +4,21 @@ const sequelize = new Sequelize; ('mysql', 'root', 'Pcezar11@*', {
     dialect:'mysql'
 })
 
-sequelize.authenticate().then(function () {
+/*sequelize.authenticate().then(function () {
     console.log('Connected to database');
 }).catch(function (err) {
     console.log("Falha ao se conectar: " + err)
+})*/
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING,
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+
+    }
 })
+
+Postagem.sync({force:true})
+
